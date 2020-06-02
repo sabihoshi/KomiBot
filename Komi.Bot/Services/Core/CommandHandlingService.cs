675fa7320ef3b582bf71c5b3d1cc1539ad395999
hizamakura <kaworinakano@gmail.com>
2020-06-02 18:34:45 +0800

    📝 Move discord events to their own listeners

--- Komi.Bot/Services/Core/CommandHandlingService.cs
+++ Komi.Bot/Services/Core/CommandHandlingService.cs
@@ -1,38 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using Discord.WebSocket;
-using Komi.Bot.Services.Utilities;
-using Komi.Data;
 
 namespace Komi.Bot.Services.Core
 {
     public class CommandHandlingService
     {
         private readonly CommandService _commands;
-        private readonly DiscordSocketClient _discord;
         private readonly IServiceProvider _services;
-        private readonly KomiContext _database;
-        private readonly Cache<ulong, IEnumerable<string>> _prefixCache;
 
         public CommandHandlingService(
             IServiceProvider services,
-            CommandService commands,
-            DiscordSocketClient discord,
-            KomiContext database)
+            CommandService commands)
         {
             _commands = commands;
-            _discord = discord;
             _services = services;
-            _database = database;
-            _prefixCache = new Cache<ulong, IEnumerable<string>>(GetPrefix);
 
             _commands.CommandExecuted += CommandExecutedAsync;
-            _discord.MessageReceived += MessageReceivedAsync;
         }
 
         public Task CommandExecutedAsync(
@@ -50,42 +36,6 @@ namespace Komi.Bot.Services.Core
             return Task.CompletedTask;
         }
 
-        public Task CommandFailedAsync(ICommandContext context, IResult result) =>
-            context.Channel.SendMessageAsync($"Error: {result.ErrorReason}");
-
         public Task InitializeAsync() => _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-
-        public async Task MessageReceivedAsync(SocketMessage rawMessage)
-        {
-            if (!(rawMessage is SocketUserMessage message))
-                return;
-
-            if (message.Source != MessageSource.User)
-                return;
-
-            var argPos = 0;
-            var context = new SocketCommandContext(_discord, message);
-            var prefixes = _prefixCache[context.Guild.Id];
-            if (!(message.HasStringPrefix("k!", ref argPos)
-               || message.HasMentionPrefix(_discord.CurrentUser, ref argPos)))
-            {
-                if (!prefixes.Any(p => message.HasStringPrefix(p, ref argPos)))
-                    return;
-            }
-
-            var result = await _commands.ExecuteAsync(context, argPos, _services);
-
-            if (!result.IsSuccess)
-                await CommandFailedAsync(context, result).ConfigureAwait(false);
-        }
-
-        private IEnumerable<string> GetPrefix(ulong guildId)
-        {
-            return _database.Groups
-                      .SingleOrDefault(g => g.GuildId == guildId)
-                     ?.GroupSettings.Prefixes
-                     ?.Select(x => x.Text)
-                ?? Enumerable.Empty<string>();
-        }
     }
 }
---
+++ Komi.Bot/Services/Core/Listeners/MessageRecievedListener.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Komi.Bot.Services.Core.Messages;
+using Komi.Bot.Services.Utilities;
+using Komi.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Komi.Bot.Services.Core
+{
+    public class MessageRecievedListener : INotificationHandler<MessageReceivedNotification>
+    {
+        private readonly CommandService _commands;
+        private readonly DiscordSocketClient _discord;
+        private readonly IServiceProvider _services;
+        private readonly Cache<ulong, IEnumerable<string>> _prefixCache;
+        private readonly KomiContext _database;
+
+        public MessageRecievedListener(CommandService commands, DiscordSocketClient discord, IServiceProvider services,
+            KomiContext database)
+        {
+            _commands = commands;
+            _discord = discord;
+            _services = services;
+            _database = database;
+
+            _prefixCache = _prefixCache = new Cache<ulong, IEnumerable<string>>(GetPrefix);
+        }
+
+        public Task CommandFailedAsync(ICommandContext context, IResult result) =>
+            context.Channel.SendMessageAsync($"Error: {result.ErrorReason}");
+
+        public async Task Handle(MessageReceivedNotification notification, CancellationToken cancellationToken)
+        {
+            var rawMessage = notification.Message;
+
+            if (!(rawMessage is SocketUserMessage message))
+                return;
+
+            if (message.Source != MessageSource.User)
+                return;
+
+            var argPos = 0;
+            var context = new SocketCommandContext(_discord, message);
+            var prefixes = _prefixCache[context.Guild.Id];
+            if (!(message.HasStringPrefix("k!", ref argPos)
+               || message.HasMentionPrefix(_discord.CurrentUser, ref argPos)))
+            {
+                if (!prefixes.Any(p => message.HasStringPrefix(p, ref argPos)))
+                    return;
+            }
+
+            var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+            if (!result.IsSuccess)
+                await CommandFailedAsync(context, result).ConfigureAwait(false);
+        }
+
+        private IEnumerable<string> GetPrefix(ulong guildId)
+        {
+            return _database.Groups
+                      .SingleOrDefault(g => g.GroupId == guildId)
+                     ?.GroupSettings.Prefixes
+                     ?.Select(x => x.Text)
+                ?? Enumerable.Empty<string>();
+        }
+    }
+}
